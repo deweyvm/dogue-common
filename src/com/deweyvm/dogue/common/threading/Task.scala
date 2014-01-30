a9a706ac839fd9141acc0137d5058ba56a29c916
deweyvm <deweyvm@gmail.com>
2014-01-30 08:01:49 -0500

    cleaned up task interface and refactored Main

--- src/com/deweyvm/dogue/common/threading/Task.scala
+++ src/com/deweyvm/dogue/common/threading/Task.scala
@@ -1,8 +1,42 @@
 package com.deweyvm.dogue.common.threading
 
+import com.deweyvm.dogue.common.logging.Log
+
 
 abstract class Task {
-  def execute():Unit
+
+  private var running = true
+
+  final def kill() {
+    running = false
+    killAux()
+  }
+
+  //used to do additional kills when this object's kill method is called
+  def killAux() {
+
+  }
+
+  def doWork():Unit
+  def cleanup():Unit
+  def isRunning = running
+
+  def logException(t:Throwable) {
+    Log.warn(Log.formatStackTrace(t))
+  }
+
+  final def execute() {
+    try {
+      while (running && isRunning) {
+        doWork()
+      }
+    } catch {
+      case t:Throwable => logException(t)
+    } finally {
+      cleanup()
+    }
+  }
+
   final def start():Task = {
     ThreadManager.spawn(this)
     this

72ce43c053c4ad2f6b5ae04a91fe3f748a838c78
deweyvm <deweyvm@gmail.com>
2014-02-11 12:23:02 -0500

    refactoring

---
+++ src/com/deweyvm/dogue/common/procgen/PerlinPoisson.scala
@@ -0,0 +1,14 @@
+package com.deweyvm.dogue.common.procgen
+
+class PerlinPoisson(val size:Int, val minDist:Int, freq:Double, octaves:Int, seed:Int) {
+  private val perlin = new PerlinNoise(freq, octaves, size, seed).lazyRender
+  val rng = new PoissonRng(size,size, {
+    case (i, j) => perlin.get(i, j).map{t =>
+      val x = t*35
+      //println(x)
+      math.max(x, minDist)
+    }.getOrElse(10)
+  }, minDist)
+
+
+}

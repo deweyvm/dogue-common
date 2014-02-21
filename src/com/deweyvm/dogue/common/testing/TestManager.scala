a518439988e22728dd7d45a99117177504fb5362
deweyvm <deweyvm@gmail.com>
2014-02-21 12:09:33 -0500

    refactored testing code

--- src/com/deweyvm/dogue/common/testing/TestManager.scala
+++ src/com/deweyvm/dogue/common/testing/TestManager.scala
@@ -20,17 +20,21 @@ object TestManager {
     tests map {case (f, n) => new Test {
       def runTests(): Unit = f()
       def name = n
-    }} foreach { test =>
-      test.run() match {
-      case TestPass => Test.outputln("[PASS] %15s" format test.name)
-      case TestFail(msg) =>
-        Test.outputln("[FAIL] %15s" format test.name)
-        if (failFirst) {
+    }} map { test =>
+      val result = test.run()
+      printResult(result, test.name)
+      result match {
+        case TestFail(msg) if failFirst =>
           throw new RuntimeException("Test failure: " + test.name + "\n" + msg)
-        }
+        case _ => ()
+      }
     }
+  }
 
-
-    }
+  private def printResult(result:TestResult, name:String) = result match {
+    case TestPass =>
+      Test.outputln("[PASS] %15s" format name)
+    case TestFail(msg) =>
+      Test.outputln("[FAIL] %15s" format name)
   }
 }
--- src/com/deweyvm/dogue/common/testing/TestResult.scala
+++ src/com/deweyvm/dogue/common/testing/TestResult.scala
@@ -1,5 +1,10 @@
 package com.deweyvm.dogue.common.testing
 
-trait TestResult
+trait TestResult {
+  def isFailure:Boolean = this match {
+    case TestPass => false
+    case _ => true
+  }
+}
 case object TestPass extends TestResult
 case class TestFail(msg:String) extends TestResult

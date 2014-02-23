a5d6b92f2d8cd63e8722b28d540f148ca7cfe166
deweyvm <deweyvm@gmail.com>
2014-02-23 08:14:17 -0500

    beginning work on nonblocking ecosphere creation

--- src/com/deweyvm/dogue/common/data/Circle.scala
+++ src/com/deweyvm/dogue/common/data/Circle.scala
@@ -2,6 +2,17 @@ package com.deweyvm.dogue.common.data
 
 import com.deweyvm.gleany.data.Point2d
 
+object Circle {
+  def ccwPath(radius:Double, period:Double, t:Double):Point2d = {
+    val a = t*Angles.Tau/period
+    radius *: Point2d(math.sin(a), math.cos(a))
+  }
+  def cwPath(radius:Double, period:Double, t:Double):Point2d = {
+    val a = t*Angles.Tau/period
+    radius *: Point2d(math.sin(-a), math.cos(a))
+  }
+}
+
 case class Circle(r:Double, center:Point2d) {
   def contains(pt:Point2d) = {
     (center - pt).magnitude2 < r*r
--- src/com/deweyvm/dogue/common/data/Meters.scala
+++ src/com/deweyvm/dogue/common/data/Meters.scala
@@ -13,27 +13,5 @@ case class Meters(private val rep:Double) {
   def <(other:Meters) = rep < other.rep
   def <=(other:Meters) = rep <= other.rep
 
-  def elevationColor(max:Meters):(Color, Code) = {
-    val h = d
-    if (h <= 0.m) {
-      val cr = Color.Blue.dim((math.abs(h)/max.d).toFloat)
-      (cr, Code.≈)
-    } else if (h < 50.m) {
-      (Color.Yellow, Code.`.`)
-    } else if (h < 750.m) {
-      (Color.Green, Code.`"`)
-    } else if (h < 5000.m) {
-      (Color.DarkGreen, Code.♠)
-    } else if (h < 6000.m) {
-      (Color.DarkGrey, Code.▲)
-    } else if (h < 7000.m) {
-      (Color.Grey, Code.▲)
-    } else if (h < 8000.m){
-      (Color.White.dim(1.3f), Code.▲)
-    } else if (h < 9000.m) {
-      (Color.White.dim(1.2f), Code.▲)
-    } else {
-      (Color.White.dim(1.1f), Code.▲)
-    }
-  }
+
 }

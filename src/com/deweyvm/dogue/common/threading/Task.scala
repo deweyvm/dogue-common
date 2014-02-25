2f78498d83a99c939b5616285562f31f4017d9dc
deweyvm <deweyvm@gmail.com>
2014-02-25 06:51:26 -0500

    closes issue #227 - clean up access privileges in Task

--- src/com/deweyvm/dogue/common/threading/Task.scala
+++ src/com/deweyvm/dogue/common/threading/Task.scala
@@ -6,34 +6,35 @@ import com.deweyvm.dogue.common.logging.Log
 abstract class Task {
   private var running = true
 
-  final def kill() {
+  final protected def kill() {
     if (running) {
       running = false
       killAux()
     }
   }
 
-  def init() {
+  protected def init() {
 
   }
 
   //used to do additional kill notifications when this object's kill method is called
-  def killAux() {
+  protected def killAux() {
 
   }
 
-  def doWork():Unit
-  def cleanup() {
+  protected def doWork():Unit
+
+  protected def cleanup() {
 
   }
 
-  def exception(t:Exception) {
+  protected def exception(t:Exception) {
 
   }
 
   def isRunning = running
 
-  def logException(t:Exception) {
+  private def logException(t:Exception) {
     Log.warn(Log.formatStackTrace(t))
   }
 

55e90a9192924bb86d540dbfad64100db9a4fd3a
deweyvm <deweyvm@gmail.com>
2014-02-25 06:32:11 -0500

    added zip function

--- src/com/deweyvm/dogue/common/data/Indexed2d.scala
+++ src/com/deweyvm/dogue/common/data/Indexed2d.scala
@@ -6,7 +6,11 @@ trait Indexed2d[+T] {
   def strictGetAll:Vector[T]//for debugging only
   def foreach(f:(Int,Int,T) => Unit):Unit
   def map[K](f:(Int, Int, T) => K):Indexed2d[K]
-
+  def zip[K](a:Indexed2d[K], default: K):Indexed2d[(T,K)] = {
+    map { case (i, j, t) =>
+      (t, a.get(i, j).getOrElse(default))
+    }
+  }
   def cut[K](c:Int, r:Int, f:T => K, default: => K):Indexed2d[K]
   def get(i:Int, j:Int):Option[T]
   def slice[K](x:Int, y:Int, width:Int, height:Int, f:T => K, default: => K):Indexed2d[K]

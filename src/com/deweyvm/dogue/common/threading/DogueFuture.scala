0ed74d27da94a042e6d92b9e34c07482f69c9f66
deweyvm <deweyvm@gmail.com>
2014-03-18 19:02:54 -0400

    cleaned up loading code

--- src/com/deweyvm/dogue/common/threading/DogueFuture.scala
+++ src/com/deweyvm/dogue/common/threading/DogueFuture.scala
@@ -4,13 +4,10 @@ import com.deweyvm.dogue.common.CommonImplicits
 import CommonImplicits._
 
 object DogueFuture {
-  def createAndRun[T](f:() => T):DogueFuture[T] = runProgress(f, () => 0.0, () => "")
-  def runProgress[T](f:() => T, g:() => Double, d:() => String):ProgressFuture[T] = {
-    val task = new Task with ProgressFuture[T] {
+  def createAndRun[T](f:() => T):DogueFuture[T] = {
+    val task = new Task with DogueFuture[T] {
       private var result:Option[T] = None
       private var failed = false
-      override def getProgress = g()
-      override def getDescription = d()
       override def doWork() {
         val res = f().some
         setResult(res)
@@ -32,9 +29,4 @@ object DogueFuture {
 trait DogueFuture[T] {
   def getResult:Option[T]
   def hasFailed:Boolean
-}
-
-trait ProgressFuture[T] extends DogueFuture[T] {
-  def getProgress:Double
-  def getDescription:String
 }

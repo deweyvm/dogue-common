4f04f8843e67cba1dd3dc267e2a7f2ad8c55694b
deweyvm <deweyvm@gmail.com>
2014-03-18 15:00:01 -0400

    progressive title loading

--- src/com/deweyvm/dogue/common/threading/DogueFuture.scala
+++ src/com/deweyvm/dogue/common/threading/DogueFuture.scala
@@ -4,12 +4,13 @@ import com.deweyvm.dogue.common.CommonImplicits
 import CommonImplicits._
 
 object DogueFuture {
-  def createAndRun[T](f:() => T):DogueFuture[T] = runProgress(f, () => 0.0)
-  def runProgress[T](f:() => T, g:() => Double):ProgressFuture[T] = {
+  def createAndRun[T](f:() => T):DogueFuture[T] = runProgress(f, () => 0.0, () => "")
+  def runProgress[T](f:() => T, g:() => Double, d:() => String):ProgressFuture[T] = {
     val task = new Task with ProgressFuture[T] {
       private var result:Option[T] = None
       private var failed = false
       override def getProgress = g()
+      override def getDescription = d()
       override def doWork() {
         val res = f().some
         setResult(res)
@@ -35,4 +36,5 @@ trait DogueFuture[T] {
 
 trait ProgressFuture[T] extends DogueFuture[T] {
   def getProgress:Double
+  def getDescription:String
 }

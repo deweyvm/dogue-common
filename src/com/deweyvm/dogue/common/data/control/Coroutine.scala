f30351a27b0ea84888efc9feb942b446f18d79c5
deweyvm <deweyvm@gmail.com>
2014-03-19 16:05:56 -0400

    refactored coroutine

--- src/com/deweyvm/dogue/common/data/control/Coroutine.scala
+++ src/com/deweyvm/dogue/common/data/control/Coroutine.scala
@@ -3,30 +3,23 @@ package com.deweyvm.dogue.common.data.control
 import com.deweyvm.gleany.data.Timer
 
 
-trait Coroutine[T] {
-  type Result
-  def apply():Result
-  /**
-   * Warning: calling this function will cause the next part of the
-   * coroutine to execute if it has not already.
-   * @return number of nanoseconds it took to run the apply method
-   */
-  def nanos:Long
-}
-case class Return[T](f:() => T) extends Coroutine[T] {
-  lazy val (t, result) = Timer.timer(f)
-  override type Result = T
-  def apply(): T = result
+trait Coroutine[T]
+case class Return[T](f:() => T) extends Coroutine[T]
 
-  /** @inheritdoc */
-  def nanos = t
+
+object Yield {
+  def unapply[T](y:Yield[T]):Option[(Int, String, () => Coroutine[T])] = {
+    Some((y.progress, y.label, y.getResult))
+  }
+
+  def apply[T](progress:Int, label:String, f:() => Coroutine[T]) = new Yield(progress, label, f)
+}
+class Yield[T](val progress:Int, val label:String, private val f:()=>Coroutine[T]) extends Coroutine[T] {
+  private def getResult = { () => {
+      val (t, result) = Timer.timer(f)
+      YieldResult(t, result)
+    }
+  }
 }
 
-case class Yield[T](progress:Int, label:String, f:()=>Coroutine[T]) extends Coroutine[T] {
-  override type Result = Coroutine[T]
-  lazy val (t, result) = Timer.timer(f)
-  def apply(): Coroutine[T] = result
-
-  /** @inheritdoc */
-  def nanos = t
-}
+case class YieldResult[T](time:Long, next:Coroutine[T]) extends Coroutine[T]

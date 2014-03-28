ef0725319e5dce7f278128683021a5f61301f183
deweyvm <deweyvm@gmail.com>
2014-03-28 09:23:15 -0400

    moved data types to smaller scope

--- src/com/deweyvm/dogue/common/package.scala
+++ src/com/deweyvm/dogue/common/package.scala
@@ -25,45 +25,7 @@ package object common {
     implicit def val2Scalar[A](t:A)(implicit n:Numeric[A])  = new Scalar(t)
     implicit def any2Range[A](self:A) = new RangeOp(self)
 
-    implicit class Meters(val d:Double) extends AnyVal {
-      def m:Meters = this
-      def f = d.toFloat
-      def >(other:Meters) = d > other.d
-      def >=(other:Meters) = d >= other.d
-      def <(other:Meters) = d < other.d
-      def <=(other:Meters) = d <= other.d
-      def +(other:Meters) = (d + other.d).m
-      def unary_- = Meters(-d)
-      override def toString = "%.2fm" format d
-    }
 
-    implicit class Pressure(val d:Double) extends AnyVal {
-      def atm:Pressure = this
-    }
-
-    implicit class Celcius(val d:Double) extends AnyVal {
-      def C:Celcius = this
-    }
-
-    /**
-     * rainfall has units of mm/year
-     * @param d
-     */
-    implicit class Rainfall(val d:Double) extends AnyVal {
-      def `mm/yr`:Rainfall = this
-    }
-
-    implicit val metersOrdered = new Ordering[Meters] {
-      def compare(m1:Meters, m2:Meters) = m1.d.compare(m2.d)
-    }
-
-    implicit val celciusOrdered = new Ordering[Celcius] {
-      def compare(c1:Celcius, c2:Celcius) = c1.d.compare(c2.d)
-    }
-
-    implicit val rainfallOrdered = new Ordering[Rainfall] {
-      def compare(r1:Rainfall, r2:Rainfall) = r1.d.compare(r2.d)
-    }
 
 
   }
